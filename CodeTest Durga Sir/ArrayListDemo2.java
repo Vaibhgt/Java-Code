7269192ccc23ea7298ec0343e81bfb60787d64a0
Vaibhgt <101324816+Vaibhgt@users.noreply.github.com>
2022-06-05 15:17:53 +0530

    Add files via upload

---
+++ CodeTest Durga Sir/ArrayListDemo2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.*;
+class ArrayListDemo2{
+	public static void main (String Args[]){
+		ArrayList l1=new ArrayList();
+		LinkedList l2=new LinkedList();
+		//System.out.println(l1 instanceof Serializable);
+		System.out.println(l2 instanceof Cloneable);
+		System.out.println(l1 instanceof RandomAccess);
+		System.out.println(l2 instanceof RandomAccess);
+		
+		
+		
+		
+	}
+	
+	
+}
---
+++ CodeTest Durga Sir/LinkedList.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class LinkedList1{
+	public static void main (String args[]){
+		LinkedList l1=new LinkedList();
+		l1.add("30");
+		l1.add("a");
+		l1.add("vaibhav");
+		l1.add("kritika");
+		
+		System.out.println(l1);
+
+		
+		
+	}
+}
